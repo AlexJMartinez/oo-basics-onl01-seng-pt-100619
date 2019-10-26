0d662f7b6f1ca4d1412eba0bae6cfb7c0b14604e
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-26 00:04:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,16 +2,16 @@ class Book
   def initialize(title)
     @title = title
   end
-  def title=(title)
-    @title = title
+  def title
+    @title
   end
-  def author=(author)
-    @author = author
+  def author
+    @author
   end
-  def page_count=(page_count)
-    @page_count = page_count
+  def page_count
+    @page_count
   end
-  def genres=(genres)
-    @genres = genres
+  def genres
+    @genres
   end
 end
