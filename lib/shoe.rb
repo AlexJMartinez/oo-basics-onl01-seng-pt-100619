16a032d93330a2facece2bd47af29ce807d68aa7
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-26 00:56:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,12 @@
 class Shoe
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
 
-  def condition
-    puts "The shoe has been repaired"
-  end
+  #def condition
+  #  puts "The shoe has been repaired"
+  #end
 end
